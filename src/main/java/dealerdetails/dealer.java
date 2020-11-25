60481c329264632c25570347843b781362629c86
Saurabh Kyatam <saurabhkyatam77@gmail.com>
2020-11-25 12:21:04 +0530

    add

--- src/main/java/dealerdetails/dealer.java
+++ src/main/java/dealerdetails/dealer.java
@@ -3,6 +3,24 @@ package dealerdetails;
 public class dealer {
 	String name,address;
 	int shopno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getShopno() {
+		return shopno;
+	}
+	public void setShopno(int shopno) {
+		this.shopno = shopno;
+	}
 	public dealer(String name, String address, int shopno) {
 		super();
 		this.name = name;
