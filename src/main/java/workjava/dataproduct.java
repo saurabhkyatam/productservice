c90078b3ec300c6c74992092c340b6570ae20e96
Saurabh Kyatam <saurabhkyatam77@gmail.com>
2020-11-25 11:48:06 +0530

    addsettergetter

--- src/main/java/workjava/dataproduct.java
+++ src/main/java/workjava/dataproduct.java
@@ -1,5 +1,27 @@
 package workjava;
 
 public class dataproduct {
+	
+	
+	int id;
+	String name;
+	public dataproduct(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
