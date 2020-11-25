a17079f0805a1d6d0255fb118eccb351462a3541
Saurabh Kyatam <saurabhkyatam77@gmail.com>
2020-11-25 12:15:58 +0530

    create setter getter

--- src/main/java/dealerdetails/dealer.java
+++ src/main/java/dealerdetails/dealer.java
@@ -3,5 +3,11 @@ package dealerdetails;
 public class dealer {
 	String name,address;
 	int shopno;
+	public dealer(String name, String address, int shopno) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.shopno = shopno;
+	}
 
 }
