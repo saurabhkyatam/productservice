77cb517709ae9df1eb2ac262fb7e520165949f75
Saurabh Kyatam <saurabhkyatam77@gmail.com>
2020-12-30 11:41:02 +0530

    ,jjjl

--- src/main/java/workjava/rungit.java
+++ src/main/java/workjava/rungit.java
@@ -5,6 +5,7 @@ public class rungit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello git");
+			System.out.println("work stash");
 	}
 
 }
